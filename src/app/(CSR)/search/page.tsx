8fdf32cae2582403e139ca809fefca1089a6b54c
Adan0104 <abidinadanul@gmail.com>
2024-04-27 21:55:44 +0530

    final project

---
+++ src/app/(CSR)/search/page.tsx
@@ -0,0 +1,10 @@
+import SearchPage from "./SearchPage"
+
+
+export const metadata = {
+    title: "Search - NextJS Image Gallery",
+}
+
+export default function Page(){
+    return <SearchPage />
+}
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,13 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
+import {Container, SSRProvider} from '@/components/bootstrap';
+import NavBar from './NavBar';
 
 const inter = Inter({ subsets: ['latin'] })
 
-export const metadata: Metadata = {
+export const metadata = {
   title: 'Nextjs image gallery',
   description: 'Nextjs practice project',
 }
@@ -16,7 +19,16 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <SSRProvider>
+          <NavBar />
+          <main>
+            <Container className='py-4'>
+            {children}
+            </Container>
+          </main>
+        </SSRProvider>
+      </body>
     </html>
   )
 }
